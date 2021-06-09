f5b299a3cab05177238aa11ca69cdbf667d99a08
Hollis <hollisjamison@gmail.com>
2021-06-09 12:15:54 -0400

    Removes comments

--- src/components/Manufacturer.js
+++ src/components/Manufacturer.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
 const Manufacturer = ({ id, name, country }) => (
-  // Use the information passed in as props (deconstructed via {} in the paramaters)
-  // to render a div holding all of our information about a manufacturer.
-  // Because this is a component it can be repeated infinitely no matter how many
-  // manufacturers we add to our database.
   <div key={id} className="manufacturer">
     {`${name} (${country})`}
   </div>
